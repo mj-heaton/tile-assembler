c4f4239ff8718e8d0aae60ec0b22671155c9f333
Matthew Heaton <matthew.heaton@bioxydyn.com>
2024-08-06 11:50:11 +0100

    Use SHA to detect duplicate tiles returned by some servers
    Some servers return duplicates of the most recent tile in-lieu of HTTP error codes

--- get-tiles.py
+++ get-tiles.py
@@ -2,6 +2,8 @@ import os
 import requests
 import argparse
 import sys
+import hashlib
+import re
 
 
 def download_jpegs(
@@ -12,6 +14,7 @@ def download_jpegs(
 ) -> None:
     if not os.path.exists(download_dir):
         os.makedirs(download_dir)
+    hashes: set[str] = set()
 
     x = 0
     while True:
@@ -33,6 +36,19 @@ def download_jpegs(
             with open(file_path, 'wb') as file:
                 file.write(response.content)
 
+            # Get the SHA256 hash of the downloaded file
+            with open(file_path, 'rb') as file:
+                file_hash = hashlib.sha256(file.read()).hexdigest()
+                print(f"SHA256 hash of {file_path}: {file_hash}")
+                # Check if the hash has already been seen
+                if file_hash in hashes:
+                    print(f"Warning: Duplicate file detected")
+                    # Remove the duplicate file
+                    os.remove(file_path)
+                    break
+
+                hashes.add(file_hash)
+
             print(f"Downloaded {file_path}")
             y += 1
 
@@ -52,7 +68,17 @@ def main() -> None:
     parser.add_argument('base_url', type=str, help='The base URL pattern (e.g., "https://example.com/images/image_").')
     parser.add_argument('download_dir', type=str, help='The directory to save the downloaded images.')
     args = parser.parse_args()
-    download_jpegs(args.base_url, args.download_dir)
+
+    # Define the regex pattern to match the final {integer}_{integer}.jpg or .jpeg part
+    pattern = r'\d+_\d+\.(jpg|jpeg)$'
+    # Use re.sub to replace the matched pattern with an empty string
+    base_url = re.sub(pattern, '', args.base_url)
+
+    # If there are no spaces in the download_dir, replace - with spaces and capitalize the first letter of each word
+    if ' ' not in args.download_dir:
+        args.download_dir = ' '.join(args.download_dir.split('-')).title()
+
+    download_jpegs(base_url, args.download_dir)
 
 if __name__ == '__main__':
     main()
